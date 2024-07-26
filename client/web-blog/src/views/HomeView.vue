c232aa5bf4509890336296191c2ed0316bbf3c31
donwen <1359822154@qq.com>
2024-07-26 18:06:28 +0800

    feat: web-blog 初始化

---
+++ client/web-blog/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    home页面{{ test }}
+    <button class="btn" @click="onclickLogin">跳转login</button>
+    <a-space wrap>
+      <a-button type="primary">Primary Button</a-button>
+      <a-button>Default Button</a-button>
+      <a-button type="dashed">Dashed Button</a-button>
+      <a-button type="text">Text Button</a-button>
+      <a-button type="link">Link Button</a-button>
+    </a-space>
+  </div>
+</template>
+<script setup lang="ts">
+import { useUserStore } from '../stores/user'
+import { useRouter } from 'vue-router'
+const store = useUserStore()
+const test = store.$state.test
+const router = useRouter()
+
+const onclickLogin = () => {
+  router.push({ name: 'login' })
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  .btn {
+    background-color: #66ccff;
+  }
+}
+</style>
---
+++ client/web-blog/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    login页面{{ test }}
+  </div>
+</template>
+<script setup lang="ts">
+import { useUserStore } from '../stores/user'
+const store = useUserStore()
+const test = store.$state.test
+</script>
+
+<style></style>
